cf4e45aae991d1f85750fc7ed8aa1ac46cf301b5
NKSnT <bassinet.nahel@gmail.com>
2023-02-13 10:41:25 +0100

    again, missed comment removal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//layout v : import { BrowserRouter as Router, Routes, Route, Outlet, Link } from "react-router-dom";
 
 import Home from './pages/Home';
 import About from './pages/About';
