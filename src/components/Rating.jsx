bb6f19ee29e3bc47e50f1da957aefd8d6ae383f2
NKSnT <bassinet.nahel@gmail.com>
2024-01-30 08:30:41 +0100

    star rating use now less code

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -10,7 +10,9 @@ import '../styles/Rating.css';
 function Rating({ rating }) {
   const star = Number(rating); //Number force the variable to be take as an number
   const [isMobile, setIsMobile] = useState(false);
+  /* const [isFUllStar, setIsFullStar] = useState; */
   //choose the screen size to make change on wanted device
+  console.log(isMobile);
   const handleResize = () => {
     if (window.innerWidth < 720) {
       setIsMobile(true);
@@ -21,51 +23,23 @@ function Rating({ rating }) {
   useEffect(() => {
     window.addEventListener('resize', handleResize);
   });
-  return (
-    <div className="">
-      {!isMobile &&
-        [...Array(star)].map((value, index) => {
-          return (
-            <span className="" key={index}>
-              <img src={star_full} alt="full star" />
-            </span>
-          );
-        })}
-      {!isMobile &&
-        [...Array(5 - star)].map((value, index) => {
-          return (
-            <span className="" key={index}>
-              <img src={star_empty} alt="empty star" />
-            </span>
-          );
-        })}
-      {isMobile &&
-        [...Array(star)].map((value, index) => {
-          return (
-            <span className="" key={index}>
-              <img src={star_full_mobile} alt="full star" />
-            </span>
-          );
-        })}
 
-      {isMobile &&
-        [...Array(5 - star)].map((value, index) => {
-          return (
-            <span className="" key={index}>
-              <img src={star_empty_mobile} alt="empty star" />
-            </span>
-          );
-        })}
-      {/* {[...Array(star)].map((value, index) => {
-        return <img src={star_full} alt="full star" key={index} />;
+  return (
+    <div>
+      {[...Array(star)].map((value, index) => {
+        return (
+          <span className="" key={index}>
+            <img src={!isMobile ? star_full : star_full_mobile} alt="full star" />
+          </span>
+        );
       })}
       {[...Array(5 - star)].map((value, index) => {
         return (
           <span className="" key={index}>
-            <img src={star_empty} alt="empty star" />
+            <img src={!isMobile ? star_empty : star_empty_mobile} alt="empty star" />
           </span>
         );
-      })} */}
+      })}
     </div>
   );
 }
